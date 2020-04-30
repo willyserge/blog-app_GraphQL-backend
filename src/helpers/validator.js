ae407c8825243ac0039f2ed10847f8bca0ebbae1
willyserge <willyserge01@gmail.com>
2020-04-30 20:52:15 +0200

    validate user login inputs

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,16 +1,24 @@
-import Joi from '@hapi/joi';
+import Joi from "@hapi/joi";
 
 const Validate = {
-    register:(userInfo) => {
-      const schema = Joi.object({
-        name: Joi.string().trim().required(),
-        email: Joi.string().trim().email().required(),
-        password: Joi.string().trim().min(6).required(),
-        confirmPassword: Joi.ref('password'),
-      });
-  
-      return schema.validate(userInfo);
-    }
-}
+  register: (userInfo) => {
+    const schema = Joi.object({
+      name: Joi.string().trim().required(),
+      email: Joi.string().trim().email().required(),
+      password: Joi.string().trim().min(6).required(),
+      confirmPassword: Joi.ref("password"),
+    });
+
+    return schema.validate(userInfo);
+  },
+  login: (userInfo) => {
+    const schema = Joi.object({
+      email: Joi.string().trim().email().required(),
+      password: Joi.string().trim().min(6).required(),
+    });
+
+    return schema.validate(userInfo);
+  },
+};
 
 export default Validate;
