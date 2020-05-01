38c21c634726149461b55cba3883a6032401e09b
willyserge <willyserge01@gmail.com>
2020-05-01 19:20:30 +0200

    delete comment by a user

--- src/graphql/resolvers/comments.js
+++ src/graphql/resolvers/comments.js
@@ -3,15 +3,14 @@ import { UserInputError } from "apollo-server";
 import Post from "../../models/post";
 
 const commentsResolver = {
-  
   Mutation: {
     createComment: async (_, { postId, body }, context) => {
       const { name } = checkAuth(context);
-      if (body.trim() === '') {
-        throw new UserInputError('Empty comment', {
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
           errors: {
-            body: 'Comment body must not empty'
-          }
+            body: "Comment body must not empty",
+          },
         });
       }
 
@@ -21,12 +20,31 @@ const commentsResolver = {
         post.comments.unshift({
           body,
           name,
-          createdAt: new Date().toISOString()
+          createdAt: new Date().toISOString(),
         });
         await post.save();
         return post;
-      } else throw new UserInputError('Post not found');
+      } else throw new UserInputError("Post not found");
     },
-  }
+    async deleteComment(_, { postId, commentId }, context) {
+      const { name } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].name === name) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+  },
 };
 export default commentsResolver;
